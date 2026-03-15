62446d0f37c3b653e8107c6cdb07f4a38f002fe8
luzale1999 <luzquispe1999@gmail.com>
2026-03-15 12:06:56 -0300

    Test de condiciones habitables de Planeta

--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 import kotlin.test.assertFalse
+
 /*
 class TodoListTest {
 
@@ -24,10 +25,30 @@ class TodoListTest {
 
 
 // Entrega 0 Tests
-/*
+
 class xPace {
 
+        @Test
+        @DisplayName("Un planeta tiene temperatura ideal para el ser humano (0°C a 40°C) y  una gravedad soportable  (entre 15 m/s² y 3 m/s²)?")
+        fun testeoPlaneta(){
+
+            //CREAMOS VARIABLE ADENTRO
+            val testPlanetaDesconocido= Planeta(
+                nombre="SegundaTierra",
+                temperaturaMedia=26.5f,
+                gravedad=10f,
+                nivelRadiacion=1,
+                hayAguaLiquida=true,
+                toxicidadAtm=0,
+                actTect=1,
+                tamaño=800,
+                distanciaAtierra=300,
+                fueAterrizado=false
+            )
+
+            assertTrue(testPlanetaDesconocido.tempIdeal())
+            assertTrue(testPlanetaDesconocido.gravedadSoportable())
+        }
+    }
 
 
-}
-*/
